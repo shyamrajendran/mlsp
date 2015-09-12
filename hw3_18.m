209386f2a0e482e115957b791d8946db933fb9a2
Shyam Rajendran <rshyam.psg@gmail.com>
2015-09-12 03:12:25 -0500

    lecture 3 39 added

--- hw3_18.m
+++ hw3_18.m
@@ -1,13 +1,11 @@
 function hw3_18()
-
-figure,imagesc(my_spectograph(64));
+input = '18.wav';
+figure,imagesc(my_spectograph(input, 64));
 title('64 window');
 
-
-figure,imagesc(my_spectograph(512));
+figure,imagesc(my_spectograph(input, 512));
 title('512 window');
 
-
-figure,imagesc(my_spectograph(2048));
+figure,imagesc(my_spectograph(input, 2048));
 title('2048 window');
 end
--- l3_39.m
+++ l3_39.m
@@ -1,4 +1,49 @@
 function l3_39()
+input = '39.wav';
+windowSize = 512;
+order = 20;
+[samples, sampleRate] = audioread('39.wav');
+spectrogram(samples, 512);
+title('Using matlab spectrogram');
+
+
+size(samples)
+filter = fir1(order, 0.8, 'high');
+samplesFiltered = conv(filter, samples);
+size(samplesFiltered);
+spectrogram(samplesFiltered, 512);
+
+
+filter = fir1(order, 0.9, 'low');
+samplesFiltered = conv(filter, samples);
+size(samplesFiltered);
+output = my_specHelper(samplesFiltered, sampleRate, windowSize);
+figure,imagesc(output);
+title('lowpass');
+
+filter = fir1(order, [0.2 0.8], 'bandpass');
+samplesFiltered = conv(filter, samples);
+size(samplesFiltered);
+output = my_specHelper(samplesFiltered, sampleRate, windowSize);
+figure,imagesc(output);
+title('bandpass');
+
+
+
+filter = fir1(order, 0.8, 'high');
+samplesFiltered = conv(filter, samples);
+size(samplesFiltered);
+output = my_specHelper(samplesFiltered, sampleRate, windowSize);
+figure,imagesc(output);
+title('high');
+
+
+filter = fir1(order, [0.2 0.8], 'stop');
+samplesFiltered = conv(filter, samples);
+size(samplesFiltered);
+output = my_specHelper(samplesFiltered, sampleRate, windowSize);
+figure,imagesc(output);
+title('band stop');
 
 end
-end
+
